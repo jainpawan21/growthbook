a12a261e2766df5e0f31084bd9cc9c44ad31225f
bttf <2374625+bttf@users.noreply.github.com>
2023-11-02 16:22:41 -0700

    get metrics config working

--- packages/back-end/src/tracing.ts
+++ packages/back-end/src/tracing.ts
@@ -1,54 +1,37 @@
-/*
- * Copyright The OpenTelemetry Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import process from "process";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-proto";
 import * as opentelemetry from "@opentelemetry/sdk-node";
-import { diag, DiagConsoleLogger } from "@opentelemetry/api";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
-// TODO do we need resource detectors?
-// import {
-//   getNodeAutoInstrumentations,
-//   getResourceDetectorsFromEnv,
-// } from "./utils";
+import { Resource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 
-diag.setLogger(
-  new DiagConsoleLogger(),
-  opentelemetry.core.getEnv().OTEL_LOG_LEVEL
-);
-
-const sdk = new opentelemetry.NodeSDK({
-  instrumentations: getNodeAutoInstrumentations({
-    "@opentelemetry/instrumentation-mongodb": {
-      enhancedDatabaseReporting: true,
-    },
-  }),
-  // resourceDetectors: getResourceDetectorsFromEnv(),
+const resource = new Resource({
+  [SemanticResourceAttributes.SERVICE_NAME]: "adnans-growthbook",
 });
 
-try {
-  sdk.start();
-  diag.info("OpenTelemetry automatic instrumentation started successfully");
-} catch (error) {
-  diag.error(
-    "Error initializing OpenTelemetry SDK. Your application is not instrumented and will not produce telemetry",
-    error
-  );
-}
+const metricReader = new PeriodicExportingMetricReader({
+  exporter: new OTLPMetricExporter(),
+  exportIntervalMillis: 1000,
+});
 
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: new OTLPTraceExporter(),
+  metricReader,
+  resource,
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start();
+
+// gracefully shut down the SDK on process exit
 process.on("SIGTERM", () => {
   sdk
     .shutdown()
-    .then(() => diag.debug("OpenTelemetry SDK terminated"))
-    .catch((error) => diag.error("Error terminating OpenTelemetry SDK", error));
+    // eslint-disable-next-line no-console
+    .then(() => console.log("Tracing terminated"))
+    // eslint-disable-next-line no-console
+    .catch((error) => console.log("Error terminating tracing", error))
+    .finally(() => process.exit(0));
 });
